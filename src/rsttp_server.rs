7516e594eaac5ad617d53054a38beef96de27ff7
sahej-dev <sarao_sahej@outlook.com>
2025-04-22 01:13:02 +0530

    fix: persistence implementation

--- src/rsttp_server.rs
+++ src/rsttp_server.rs
@@ -94,82 +94,53 @@ impl<Ctx: Send + Sync + std::fmt::Debug> RsttpServer<Ctx> {
     fn tcp_event_handler(&self, socket_addr: SocketAddr, server: &RsttpServer<Ctx>) {
         println!("handling for {}", socket_addr);
 
-        let mut has_set_timeout: bool = false;
+        let stream = if let Ok(mut connections) = self.peer_connections.lock() {
+            match connections.get(&socket_addr) {
+                Some(stream) => {
+                    if let Err(e) =
+                        stream.set_read_timeout(Some(server.config.persist_connection_for))
+                    {
+                        error!(error = e.to_string(), "Failed to set read timeout");
+                        connections.remove(&socket_addr);
+                        return;
+                    }
+
+                    stream.try_clone().ok()
+                }
+                None => None,
+            }
+        } else {
+            error!("Could not obtain lock on peer connections");
+            return;
+        };
+
+        let stream = match stream {
+            Some(s) => s,
+            None => {
+                error!("Could not extract stream handle");
+                return;
+            }
+        };
+
         let mut keep_alive: bool = true;
 
         while keep_alive {
-            let req = if let Ok(mut connections) = self.peer_connections.lock() {
-                println!("got connections map for {}", socket_addr);
-                if let Some(stream) = connections.get(&socket_addr) {
-                    if !has_set_timeout {
-                        has_set_timeout = true;
-                        if let Err(e) =
-                            stream.set_read_timeout(Some(server.config.persist_connection_for))
-                        {
-                            error!(error = e.to_string(), "Failed to set read timeout");
-                            return;
-                        }
+            let req = match self.get_request_from_stream(&stream) {
+                Ok(req) => req,
+                Err(e) => {
+                    if e != "Connection closed by client" {
+                        println!("bad request at error: {}", e);
+                        Self::respond(&stream, Response::bad_request());
                     }
-
-                    println!("got stream for {}", socket_addr);
-                    info!("accepted new connection");
-                    let req = match self.get_request_from_stream(stream) {
-                        Ok(req) => req,
-                        Err(e) => {
-                            if e != "Connection closed by client" {
-                                println!("bad request at error: {}", e);
-                                Self::respond(stream, Response::bad_request());
-                            } else {
-                                connections.remove(&socket_addr);
-                            }
-                            return;
-                        }
-                    };
-
-                    Ok(req)
-                } else {
-                    keep_alive = false;
-                    Err(format!(
-                        "Could not find a connection corresponding to the socket addr {}",
-                        socket_addr
-                    ))
-                }
-            } else {
-                keep_alive = false;
-                Err(String::from(
-                    "Could not obtain lock on peer connections map mutex",
-                ))
-            };
-
-            match req {
-                Ok(req) => {
-                    println!("Got Request");
-
-                    keep_alive = !req.has_connection_close_header();
-
-                    let response: Response = server.router.handle_request(req, &server.config.ctx);
-
-                    println!("Got Response");
-
-                    if let Ok(connections) = self.peer_connections.lock() {
-                        println!("got connections map for {}", socket_addr);
-                        if let Some(stream) = connections.get(&socket_addr) {
-                            println!("got stream for {}", socket_addr);
-                            Self::respond(stream, response);
-                        }
-                    }
-                }
-                Err(_) => {
-                    if let Ok(connections) = self.peer_connections.lock() {
-                        println!("got connections map for {}", socket_addr);
-                        if let Some(stream) = connections.get(&socket_addr) {
-                            println!("bad request at addr {}", socket_addr);
-                            Self::respond(stream, Response::bad_request());
-                            keep_alive = false;
-                        }
-                    }
+                    break;
                 }
             };
+
+            keep_alive = !req.has_connection_close_header();
+
+            let response: Response = server.router.handle_request(req, &server.config.ctx);
+
+            Self::respond(&stream, response);
         }
 
         if let Ok(mut connections) = self.peer_connections.lock() {
